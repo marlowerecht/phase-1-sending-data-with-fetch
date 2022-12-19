179ff09c377c1d90042a57b5fce4220cea2816af
Marlowe Recht <recht.marlowe@gmail.com>
2022-12-19 01:13:12 +0100

    commit tree 1099aadd33c32bc04be4a211f90dde76597ce575

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your code here
+// // Add your code here
+
+// const formData = {
+//     dogName: "Lilly",
+//     dogBreed: "Saint Bernard"
+// };
+
+// const configurationObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObj)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => {
+//         alert("uh oh")
+//         console.log(error.message)
+//     })
+
+function submitData(name, email) {
+
+    const dataObj = {
+        name: name.textContent,
+        email: email.textContent
+    }
+
+    // console.log(email);
+
+    // const body = document.querySelector('body');
+    // const div = document.createElement('div');
+    // body.append(div);
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name.textContent,
+            email: email.textContent
+        })
+    })
+    .then(response => response.json())
+    .then(data => document.append(data))
+}
